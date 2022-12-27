3febaaffc80a6ea5efa0e3a9c78e60dd4066c180
David Connelly <david.webguy@gmail.com>
2022-12-27 15:20:51 +0000

    added new encryption class - not yet tested

--- modules/encryption/controllers/Encryption.php
+++ modules/encryption/controllers/Encryption.php
@@ -1,93 +1,23 @@
 <?php
 class Encryption extends Trongate {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    private $key = 'TMGTXGseNPOpVU3ELDuTjxGXxZmmvG6eJ5lzaP5U7ZyH5+mfbDo/uKXg/aJWtDgqpoYTGnQerwOMLtcHCbsClg==';
-    private $cipher = "aes-128-gcm";
-    private $options = 0; // a bitwise disjunction of the flags OPENSSL_RAW_DATA and OPENSSL_ZERO_PADDING (obviously!)
-
-    function make_key() {
-        if (ENV == 'dev') {
-            echo base64_encode(openssl_random_pseudo_bytes(64));
-        }
+    function _encrypt($string) {
+        $key = openssl_random_pseudo_bytes(16);
+        $nonce = openssl_random_pseudo_bytes(12);
+        $ciphertext = openssl_encrypt($string, 'chacha20-poly1305', $key, OPENSSL_RAW_DATA, $nonce);
+        return array(
+            'encrypted_string' => base64_encode($ciphertext),
+            'key' => base64_encode($key),
+            'nonce' => base64_encode($nonce)
+        );
     }
 
-     function _encrypt($plaintext) {
-        $ivlen = \openssl_cipher_iv_length($this->cipher);
-        $iv = \openssl_random_pseudo_bytes($ivlen);
-        $ciphertext = openssl_encrypt($plaintext, $this->cipher, $this->key, $this->options, $iv,$tag);
-        $enc_string = bin2hex($iv).bin2hex($tag).$ciphertext;
-        return $enc_string;
-    }
-
-    function _decrypt($enc_string) {
-        $iv = substr($enc_string, 0, 24);
-        $tag = substr($enc_string, 24, 32);
-        $ciphertext = substr($enc_string, 56, strlen($enc_string));
-        $result = \openssl_decrypt($ciphertext, $this->cipher, $this->key, $this->options, \hex2bin($iv), \hex2bin($tag));
-        return $result;
+    function _decrypt($encrypted_string, $key, $nonce) {
+        $encrypted_string = base64_decode($encrypted_string);
+        $key = base64_decode($key);
+        $nonce = base64_decode($nonce);
+        $plaintext = openssl_decrypt($encrypted_string, 'chacha20-poly1305', $key, OPENSSL_RAW_DATA, $nonce);
+        return $plaintext;
     }
 
 }
